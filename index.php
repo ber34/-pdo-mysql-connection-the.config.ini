7176d38a8bc3bafdd22f55286ab37d588c1ba1ae
ber34 <ber34@o2.pl>
2013-08-31 22:53:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
- $db = new DataBase(); // Connect to the database.
+ $db = new DataBase();  /*Connect to the database.*/
  $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
        $stmt = $db->prepare('SELECT u_user FROM `'.$db->dbprefix().'chat` ORDER BY `u_id` ASC');
        $stmt->execute();
